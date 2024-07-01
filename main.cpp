a98bf446d030e73da3821c9e17899b8e074e5a0c
Franek <cieslik.franek@gmail.com>
2024-07-01 20:49:59 +0000

    create base functions to set terminal's flags

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "my_function.h"
 
 int main()
 {
-    std::cout<< "\nHello World!\n";
+    struct termios orig_termios;
+    enableRawMode(&orig_termios);
+      while (true) {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+        if (iscntrl(c)) {
+            std::cout << static_cast<int>(c) << "\r\n";
+        } else {
+            std::cout << static_cast<int>(c) << " ('" << c << "')\r\n";
+        }
+        if(static_cast<int>(c) == 27) break;
+    };
+    disableRawMode(&orig_termios);
+    return EXIT_SUCCESS;
 }
