f3ac63af190b6ed92e75be88285c60c71e5a8c24
Franek <cieslik.franek@gmail.com>
2024-07-01 22:37:05 +0000

    update: clear terminal, fix exit

--- main.cpp
+++ main.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 #include "my_function.h"
 
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 int main()
 {
     struct termios orig_termios;
     enableRawMode(&orig_termios);
-      while (true) {
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-        if (iscntrl(c)) {
-            std::cout << static_cast<int>(c) << "\r\n";
-        } else {
-            std::cout << static_cast<int>(c) << " ('" << c << "')\r\n";
-        }
-        if(static_cast<int>(c) == 27) break;
+    while (true) 
+    {
+       editorRefreshScreen();
+       if(editorProcessKeypress()) break;
     };
+    editorRefreshScreen();
     disableRawMode(&orig_termios);
     return EXIT_SUCCESS;
 }
--- my_function.h
+++ my_function.h
@@ -2,24 +2,43 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-void die(const char *s) {
-  perror(s);
-  exit(1);
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 void disableRawMode(struct termios* orig_termios) {
-  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, orig_termios) == -1)
-     die("tcsetattr");
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, orig_termios);
 }
 
 void enableRawMode(struct termios * orig_termios) {
-    if(tcgetattr(STDIN_FILENO, orig_termios) == -1) die("tcgetattr");
+    tcgetattr(STDIN_FILENO, orig_termios);
     struct termios raw = *orig_termios;
-     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-     raw.c_oflag &= ~(OPOST);
-     raw.c_cflag |= (CS8);
-     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-     raw.c_cc[VMIN] = 0;
-     raw.c_cc[VTIME] = 1;
-    if( tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
+
+char editorReadKey()
+{
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1);
+    return c;
+}
+
+bool editorProcessKeypress() {
+  char c = editorReadKey();
+  switch (c) {
+    case 27:
+      editorRefreshScreen();
+      return true;
+      break;
+  }
+  return false;
+}
+
